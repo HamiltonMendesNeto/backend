ad555b843d52c8bf5ea5c1cdb09e7aee81c9a1b3
HamiltonMendesNeto <hamilton.neto.senai404@gmail.com>
2024-08-02 09:43:27 -0300

    Add files via upload

---
+++ PHP/ola_mundo.php
@@ -0,0 +1,18 @@
+<?php
+//Todas as variaveis declaradas no codigo estão abaixo
+
+$nome = 'Hamilton '; 
+$escola = 'SESI Cerquilho';  
+$CE = 428; 
+$curso = 'Desenvolvimento de Sistemas'; 
+$media = 9.7; 
+
+echo $nome, '<br>';
+echo $escola, '<br>';
+echo $CE, '<br>';
+echo $curso, '<br>';
+echo $media, '<br>';
+
+
+
+?>
