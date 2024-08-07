b95903f2b2a8e20169b64f83126f9d025429efbf
HamiltonMendesNeto <hamilton.neto.senai404@gmail.com>
2024-08-07 14:44:11 -0300

    Add files via upload

---
+++ PHP/constante.php
@@ -0,0 +1,25 @@
+<?php
+
+//declarações das variaveis constantes 
+
+define('BD_URL', 'endereco_bd_dev');
+
+define('BD_USUARIO', 'usuario_dev');
+
+define('BD_SENHA','senha_dev');
+//escrevendo as variaveis constantes
+
+echo BD_URL . '<br/>';
+
+echo BD_USUARIO . '<br/>';
+
+echo BD_SENHA . '<br/>';
+
+
+
+
+
+
+
+
+?>
