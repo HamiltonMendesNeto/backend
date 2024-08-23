62bd7c820dd3016711cf6334b77648f02014426e
HamiltonMendesNeto <hamilton.neto.senai404@gmail.com>
2024-08-23 09:35:18 -0300

    Add files via upload

---
+++ PHP/9exercicio.php
@@ -0,0 +1,16 @@
+<?php
+
+
+//O for irá repetir o $i até que ele seja 50 e irá somar o valor atual ao anterior
+for($i= 1; $i<=50; $i++){
+
+    //$raiz irá pegar o valor de $i e então fará a raiz quadrada do mesmo
+    $raiz= sqrt($i);
+
+
+
+
+    echo"A reiz quadrada de $i é: $raiz <br/>" ;
+}
+
+?>
